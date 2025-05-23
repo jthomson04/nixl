397dde6b9a694334d3d31c191d73af99c36699e9
Thomas Vegas <tvegas@nvidia.com>
2025-05-23 14:28:07 +0300

    LISTENER: Add connection creation for invalidation message
    Signed-off-by: Thomas Vegas <tvegas@nvidia.com>

--- src/core/nixl_listener.cpp
+++ src/core/nixl_listener.cpp
@@ -423,11 +423,12 @@ void nixlAgentData::commWorker(nixlAgent* myAgent){
             int client_fd;
 
             // not connected
-            if(req_command < SOCK_MAX) {
-                if(client == remoteSockets.end()) {
+            if (req_command < SOCK_MAX) {
+                if (client == remoteSockets.end()) {
                     int new_client = connectToIP(req_ip, req_port);
-                    if(new_client == -1) {
-                        NIXL_ERROR << "Listener thread could not connect to IP " << req_ip << " and port " << req_port;
+                    if (new_client == -1) {
+                        NIXL_ERROR << "Listener thread could not connect to IP " << req_ip
+                                   << " and port " << req_port;
                         continue;
                     }
                     remoteSockets[req_sock] = new_client;
@@ -438,21 +439,18 @@ void nixlAgentData::commWorker(nixlAgent* myAgent){
             }
 
             switch(req_command) {
-                case SOCK_SEND:
-                {
-                    sendCommMessage(client_fd, std::string("NIXLCOMM:LOAD" + my_MD));
-                    break;
-                }
-                case SOCK_FETCH:
-                {
-                    sendCommMessage(client_fd, std::string("NIXLCOMM:SEND"));
-                    break;
-                }
-                case SOCK_INVAL:
-                {
-                    sendCommMessage(client_fd, std::string("NIXLCOMM:INVL") + name);
-                    break;
-                }
+            case SOCK_SEND: {
+                sendCommMessage(client_fd, std::string("NIXLCOMM:LOAD" + my_MD));
+                break;
+            }
+            case SOCK_FETCH: {
+                sendCommMessage(client_fd, std::string("NIXLCOMM:SEND"));
+                break;
+            }
+            case SOCK_INVAL: {
+                sendCommMessage(client_fd, std::string("NIXLCOMM:INVL") + name);
+                break;
+            }
 #if HAVE_ETCD
                 // ETCD operations using existing methods
                 case ETCD_SEND:
